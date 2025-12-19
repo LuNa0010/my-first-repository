a075b0426ddfe1c6136109d4caad9fdaaee25a41
LuNa0010 <whyuleft0000@gmail.com>
2025-12-19 21:10:25 +0800

    富翁複利計算 被當傻子薅羊毛

--- 學校指定考 意外翻車3 少一個空格.cpp
+++ 學校指定考 意外翻車3 少一個空格.cpp
@@ -1,33 +1,24 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<math.h>
-
+void acc
+{ 
+    float x=0.01;
+    for(int i=0;i<=31;i++)
+    { 
+      x=1.0*x;
+    }
+    printf("陌生人要給富豪:%.0f\n",x);
+    int a=0,sum=0;
+    while(a<31)
+    {
+        sum+=100*100;
+        a++;
+    }
+    printf("富豪要給陌生人:%d\n",sum);
+}
 int main(void)
-{   int a;
-    scanf("%d",&a);
-    printf("N=%d\n",a);
-    int totala=0;
-    int output=0;
-    if(a%2==0)
-    {
-    	for(int i=1;i<=a/2;i++)
-    	{
-    		totala++;
-    		totala++;
-    		output+=totala; 
-			printf("%d\n",totala);
-        }
-        printf("%d\n",output);
-    	
-    }
-    else if(a%2==1)
-    {
-    	int total=-1;
-    	for(int j=-1;j<a/2;j++)
-    	{
-    		total++;
-    		total++;
-    		output+=total; 
-		}
-    	printf("%d",output);
-    }
+{
+    acc(void);
+    return 0;   
 }
